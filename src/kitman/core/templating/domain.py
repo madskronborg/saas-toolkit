f724c1c63d298ceda8a8fd9c76a1d8278c6090c4
mads <madskronborg99@gmail.com>
2022-04-30 22:30:53 +0200

    Add depends_on

--- src/kitman/core/templating/domain.py
+++ src/kitman/core/templating/domain.py
@@ -1,7 +1,9 @@
 from typing import Generic, List, Literal, TypeVar
 
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, root_validator, validator
 from pydantic.generics import GenericModel
+
+from kitman.core import dynamic
 
 TTemplateVariable = TypeVar("TTemplateVariable", bound="BaseTemplateVariable")
 TTemplateItem = TypeVar("TTemplateItem", bound="BaseTemplateItem")
@@ -22,20 +24,64 @@ class BaseTemplateVariable(BaseModel):
     name: str | int
     value: str | int | None = None
     required: bool = False
+    depends_on: set[str] | None = None
     template: str | int | None = None
     group: str | int | None = None
+
+    @validator("depends_on", pre=True)
+    def validate_depends_on(cls, value: set[str] | None, values: dict, **kwargs):
+
+        depends_on = set()
+
+        variable_value = values.get("value", None)
+
+        if variable_value:
+
+            if isinstance(variable_value, str):
+                placeholders = dynamic.get_placeholders_from_str(variable_value)
+
+                if placeholders:
+                    for placeholder in placeholders:
+                        depends_on.add(placeholder)
+
+        return depends_on
 
 
 class BaseTemplateItem(GenericModel, Generic[TTemplate]):
 
     name: str | int | None = None
     value: dict
+    depends_on: set[str] | None = None
     template: str | int | None = None
+
+    @validator("depends_on", pre=True)
+    def validate_depends_on(cls, value: set[str] | None, values: dict, **kwargs):
+
+        depends_on = set()
+
+        item_value = values["value"]
+
+        if item_value:
+            for key, val in item_value:
+
+                if isinstance(key, str):
+                    key_placeholders = dynamic.get_placeholders_from_str(key)
+
+                    for key_placeholder in key_placeholders:
+                        depends_on.add(key_placeholder)
+
+                if isinstance(val, str):
+                    val_placeholders = dynamic.get_placeholders_from_str(val)
+
+                    for val_placeholder in val_placeholders:
+                        depends_on.add(val_placeholder)
+
+        return depends_on
 
 
 class BaseTemplate(GenericModel, Generic[TTemplate, TTemplateItem, TTemplateVariable]):
 
-    name: str | int = None
+    name: str | int
     category: str = "default"
     items: list[TTemplateItem] = []
     variables: list[TTemplateVariable] = []
@@ -65,10 +111,10 @@ class BaseTemplateGroup(
     GenericModel, Generic[TTemplateGroup, TTemplate, TTemplateVariable]
 ):
 
-    name: str | int = None
+    name: str | int
     templates: list[TTemplate]
     variables: list[TTemplateVariable] = []
-    children: List[TTemplateGroup] | None = None
+    children: List[TTemplateGroup] = []
 
     # Internal context variables
     extends: TTemplateGroup | None = None
@@ -83,25 +129,18 @@ class BaseTemplateGroup(
         return v
 
 
-class BaseTemplateStructureItem(
+class BaseTemplateStructure(
     GenericModel, Generic[TTemplate, TTemplateItem, TTemplateVariable]
 ):
-
-    data: TTemplate | TTemplateItem | TTemplateVariable
-    added_by_name: str | int
-    added_by_type: Literal["user"] | Literal["group"] | Literal["template"]
-
-
-class BaseTemplateStructure(GenericModel, Generic[TTemplateStructureItem]):
     """
     TemplateStructure
 
     The first item in each list is of lowest importance and can be overwritten by items later on in the list.
     """
 
-    templates: list[TTemplateStructureItem]
-    items: list[TTemplateStructureItem]
-    variables: list[TTemplateStructureItem]
+    templates: list[TTemplate]
+    items: list[TTemplateItem]
+    variables: list[TTemplateVariable]
 
 
 class BaseTemplateBuild(Generic[TTemplateBuildData, TTemplateStructure]):
