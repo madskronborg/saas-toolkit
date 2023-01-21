8a6996ff38ccff5a180c7b712ac69b19f5d58010
mads <madskronborg99@gmail.com>
2023-01-21 11:08:06 +0100

    Add beanie integration

--- src/kitman/core/domain.py
+++ src/kitman/core/domain.py
@@ -11,6 +11,10 @@ from typing import (
 from uuid import UUID
 
 from ordered_set import TypeVar
+from pydantic import BaseModel, Field, validator
+from pydantic.generics import GenericModel
+import datetime
+from uuid import uuid4
 
 # Value objects
 OpenAPIResponseType = dict[int, str, dict[str, Any]]
@@ -38,3 +42,39 @@ class Location(str, enum.Enum):
     header = "header"
     query = "query"
     cookie = "cookie"
+
+
+# Entities
+class Entity(BaseModel):
+
+    uid: UUID = Field(default_factory=uuid4)
+
+
+class TimestampedEntity(Entity):
+    created: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
+    updated: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
+
+    @validator("updated", always=True)
+    def set_updated(cls, value: datetime.datetime | None, **kwargs):
+
+        return datetime.datetime.utcnow()
+
+
+# Schemas
+class Schema(BaseModel):
+    class Config:
+        orm_mode = True
+
+
+# Pages
+TPageItem = TypeVar("TPageItem", bound=Schema)
+
+
+class Page(GenericModel, Generic[TPageItem]):
+
+    page: int = 1
+    size: int = 100
+    total: int
+    items: list[TPageItem]
+    prev_page: int | None = None
+    next_page: int | None = None
